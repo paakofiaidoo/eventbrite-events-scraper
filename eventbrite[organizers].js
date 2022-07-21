6c8793b4853d7cc1205f8c99ffce92664341e330
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-21 01:56:39 +0000

    completed with eventbrite script

--- eventbrite[organizers].js
+++ eventbrite[organizers].js
@@ -2,73 +2,82 @@ const axios = require("axios");
 // const { Parser } = require("json2csv");
 const { parse } = require("json2csv");
 const fs = require("fs");
-//https://www.eventbrite.co.uk/org/41350641393/showmore/?page_size=30&type=future&page=1
-let event = "https://www.eventbrite.co.uk/e/plant-based-social-tickets-294734888987";
 
-const getData = async (page = 1, organization_id, type) => {
-    let future, past;
+let future = [],
+    past = [];
+const getData = async (page, organization_id, type) => {
+    let temp = [];
     // @ts-ignore
     let data = await axios(`https://www.eventbrite.co.uk/org/${organization_id}/showmore/?page_size=30&type=${type}&page=${page}`)
         .then((response) => {
-            // When the page is loaded convert it to text
-            let html = response.data;
-            let startString = `<a id="organizer-link-org-panel" href="`;
-            let start = html.search(startString);
-            html = html.slice(start + startString.length);
-            let end = html.search(`"`);
-            let url = html.slice(0, end);
-            console.log(url);
-
-            return url;
+            temp.push(1);
+            if (type === "future") {
+                future.push(...response.data.data.events);
+            } else {
+                past.push(
+                    ...response.data.data.events.filter((event) => {
+                        console.log(getMonths(event.start.local),getMonths(event.start.local) <= 12);
+                        return getMonths(event.start.local) <= 12;
+                    })
+                );
+            }
+            if (response.data.data.has_next_page) {
+                temp.push(page + 1);
+                getData(page + 1, organization_id, type);
+            }
+            return response.data.data.events;
         })
-        .then((url) => {
-            // @ts-ignore
-            axios(url)
-                .then((response) => {
-                    let html = response.data;
-                    let startString = `window.__SERVER_DATA__ =`;
-                    let start = html.search(startString);
-                    let end = html.search(`window.__REACT_QUERY_STATE__`);
-                    let data = html.slice(start + startString.length, end);
-                    data = data.slice(0, data.lastIndexOf(`;`));
-                    data = JSON.parse(data);
-                    data = data["view_data"].events;
-                    return data;
-                })
-                .then((data) => {
-                    const fields = [
-                        "id",
-                        "name.text",
-                        "organizer.name",
-                        "category.name",
-                        "format.name",
-                        "start.local",
-                        "venue.name",
-                        "venue.address.localized_address_display",
-                        "venue.longitude",
-                        "venue.latitude",
-                        "summary",
-                        "organization_id",
-                    ];
-                    const opts = { fields };
-                    try {
-                         future = parse(data.future_events, opts);
-                        fs.writeFileSync("eventbrite-Future(organization=Bohemia Place Market).csv", future);
-
-                         past = parse(data.past_events, opts);
-                        fs.writeFileSync("eventbrite-Past(organization=Bohemia Place Market).csv", past);
-                    } catch (err) {
-                        console.error(err);
-                    }
-                })
-                .catch((error) => {
-                    console.log("Failed to fetch page: ", error);
-                });
+        .then((data) => {
+            temp.pop();
+            if (temp.length === 0) {
+                const fields = [
+                    "id",
+                    "name.text",
+                    "organizer.name",
+                    "category.name",
+                    "format.name",
+                    "start.local",
+                    "venue.name",
+                    "venue.address.localized_address_display",
+                    "venue.longitude",
+                    "venue.latitude",
+                    "summary",
+                    "organization_id",
+                ];
+                const opts = { fields };
+                if (type === "future") {
+                    let futureCSV = parse(future, opts);
+                    fs.writeFileSync(`eventbrite-Future(organization=${data[0].organizer.name}).csv`, futureCSV);
+                    console.log(future.length);
+                } else {
+                    let pastCSV = parse(past, opts);
+                    fs.writeFileSync(`eventbrite-Past(organization=${data[0].organizer.name}).csv`, pastCSV);
+                    console.log(past.length);
+                }
+            }
         })
-        .catch(function (err) {
-            console.log("Failed to fetch page: ", err);
+
+        .catch((error) => {
+            console.log("Failed to fetch page: ", error);
         });
     return data;
 };
 
-getData();
+
+
+//get the diffrence in months between two dates
+const getMonths = (start) => {
+    let months = [];
+    let startDate = new Date(start);
+    let endDate = new Date();
+    let currentDate = startDate;
+    while (currentDate <= endDate) {
+        months.push(new Date(currentDate));
+        currentDate.setMonth(currentDate.getMonth() + 1);
+    }
+    return months.length;
+};
+
+// insect the id here
+getData(1, "20229697518", "future");
+getData(1, "20229697518", "past");
