34088d144ca35bd166daac4185b3807ea081ebe2
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-21 03:00:51 +0000

    added comments

--- eventbrite[organizers].js
+++ eventbrite[organizers].js
@@ -1,28 +1,33 @@
 const axios = require("axios");
-// const { Parser } = require("json2csv");
 const { parse } = require("json2csv");
 const fs = require("fs");
 
 let future = [],
     past = [];
+    
 const getData = async (page, organization_id, type) => {
     let temp = [];
     // @ts-ignore
+    // get the data from the eventbrite api
     let data = await axios(`https://www.eventbrite.co.uk/org/${organization_id}/showmore/?page_size=30&type=${type}&page=${page}`)
         .then((response) => {
             temp.push(1);
             if (type === "future") {
+                // push the data to the future array
                 future.push(...response.data.data.events);
             } else {
                 past.push(
+                    // push the data to the past array
                     ...response.data.data.events.filter((event) => {
-                        console.log(getMonths(event.start.local),getMonths(event.start.local) <= 12);
+                        // filter out events that are 12 months or more in the past
                         return getMonths(event.start.local) <= 12;
                     })
                 );
             }
+            //check if there are more pages of data
             if (response.data.data.has_next_page) {
                 temp.push(page + 1);
+                // get the next page of data
                 getData(page + 1, organization_id, type);
             }
             return response.data.data.events;
@@ -30,6 +35,7 @@ const getData = async (page, organization_id, type) => {
         .then((data) => {
             temp.pop();
             if (temp.length === 0) {
+                // headers for the csv file
                 const fields = [
                     "id",
                     "name.text",
@@ -46,10 +52,12 @@ const getData = async (page, organization_id, type) => {
                 ];
                 const opts = { fields };
                 if (type === "future") {
+                    //create a csv file with the future events
                     let futureCSV = parse(future, opts);
                     fs.writeFileSync(`eventbrite-Future(organization=${data[0].organizer.name}).csv`, futureCSV);
                     console.log(future.length);
                 } else {
+                    // create a csv file with the past events
                     let pastCSV = parse(past, opts);
                     fs.writeFileSync(`eventbrite-Past(organization=${data[0].organizer.name}).csv`, pastCSV);
                     console.log(past.length);
@@ -65,7 +73,7 @@ const getData = async (page, organization_id, type) => {
 
 
 
-//get the diffrence in months between two dates
+//get the difference in months between two dates
 const getMonths = (start) => {
     let months = [];
     let startDate = new Date(start);
