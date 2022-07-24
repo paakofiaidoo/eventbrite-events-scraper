150ebff03e2309bbda6f306458aa9ffbfc83e7c9
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-07-24 03:30:54 +0000

    done with eventbrite

--- eventbrite/filtering.js
+++ eventbrite/filtering.js
@@ -1,33 +1,34 @@
 const fs = require("fs");
 const { parse } = require("json2csv");
 
-let filter = "Gaming";
-let filterPoints = ["game", "gaming", "games"];
+let filter = "media";
+let filterPoints = ["media","Public relations","Public relation"];
 
 //open the file and read it
-let events = JSON.parse(fs.readFileSync("eventbrite.json", "utf8"));
-let filtered = events
-    // .filter((event) => {
-    //     try {
-    //         return filterPoints.some((point) => {
-    //             return (
-    //                 (event.name && event.name.toLowerCase().includes(point)) ||
-    //                 (typeof event.description === "string" && event.description.toLowerCase().includes(point)) ||
-    //                 (event.category && event.category.name.toLowerCase().includes(point)) ||
-    //                 (event.summary && event.summary.toLowerCase().includes(point))
-    //             );
-    //         });
-    //     } catch (error) {
-    //         console.log(error, event);
-    //     }
-    // })
-    .filter((event) => {
-        try {
-            return (event.venue && event.venue.address.country.toLowerCase() === "gb") || (event.venue && event.venue.city === "london");
-        } catch (error) {
-            console.log(error, event);
-        }
-    });
+let events = JSON.parse(fs.readFileSync("london-events.json", "utf8"));
+let filtered = events.filter((event) => {
+    try {
+        return filterPoints.some((point) => {
+            return (
+                (event.name && event.name.toLowerCase().includes(point.toLowerCase())) ||
+                (typeof event.description === "string" && event.description.toLowerCase().includes(point.toLowerCase())) ||
+                (event.category && event.category.name.toLowerCase().includes(point.toLowerCase())) ||
+                (event.summary && event.summary.toLowerCase().includes(point.toLowerCase()))
+            );
+        });
+    } catch (error) {
+        console.log(error, event);
+    }
+});
+// .filter((event) => {
+//     try {
+//         return event.venue && event.venue.address.country.toLowerCase() === "gb" && event.venue.address.city && event.venue.address.city.toLowerCase().includes("london");
+//     } catch (error) {
+//         console.log(error, event);
+//     }
+// });
+// fs.writeFileSync(`london-events.json`, JSON.stringify(filtered));
+
 // Gaming / games design / Serious or educational games
 // Film / TV / video production / Augmented Reality
 // Theatre / arts festival / community arts
@@ -35,5 +36,5 @@ let filtered = events
 // Publishing / creative writing /
 // PR / media
 // Heritage venues
-fs.writeFileSync(`london-events.json`, JSON.stringify(filtered));
+fs.writeFileSync(`${filter}-events.json`, JSON.stringify(filtered));
 console.log(filtered.length);
